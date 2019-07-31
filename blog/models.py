4d984c471b25b04d8e356690b0343ad475ec2b85
unknown <houma77@gmail.com>
2019-07-31 16:39:41 +0900

    password5

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,11 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
-    password = models.CharField(max_length=40)
+    #password = models.CharField(max_length=40)
     def __str__(self):
         return self.title
 
     def summary(self):
         return self.body[:100]
+    
+    
--- blog/views.py
+++ blog/views.py
@@ -23,8 +23,8 @@ def error(request):
 
 def create(request):
     blog = Blog()
-    if blog.password != '1234':
-        return render(request, 'blog/error.html')
+    #if blog.password != '1234':
+        #return render(request, 'blog/error.html')
     blog.title = request.GET['title']
     blog.body = request.GET['body']
     blog.pub_date = timezone.datetime.now()
